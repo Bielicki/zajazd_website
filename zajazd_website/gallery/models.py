492d48f1e2c77b53aa0b47b494eb2481633280a1
Rafał <SadiXD@Rafas-MacBook-Pro.local>
2017-09-04 16:16:21 +0200

    Added Gallery models and views

--- zajazd_website/gallery/models.py
+++ zajazd_website/gallery/models.py
@@ -1,3 +1,96 @@
+import os
+from io import BytesIO
+
+from PIL import Image
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+
+    title = models.CharField(max_length=32)
+
+    image = models.ImageField(upload_to='gallery',
+                              null=False,
+                              blank=False)
+
+    added = models.DateTimeField(auto_now_add=True)
+
+    album = models.ForeignKey('PhotoAlbum',
+                              blank=True,
+                              null=True,
+                              related_name="photos",
+                              on_delete=models.SET_NULL)
+
+    thumbnail = models.ImageField(null=True,
+                                  upload_to='gallery_thumbnails')
+
+    class Meta:
+        ordering = ["-added"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        update = False
+        if self.id:
+            update = True
+        self.create_thumbnail()
+
+        super(Photo, self).save(*args, **kwargs, force_update=update)
+
+    def create_thumbnail(self):
+
+        if not self.image:
+            return
+        if self.thumbnail:
+            return
+
+        img_type = self.image.file.content_type
+
+        if img_type == 'image/jpeg':
+            pil_type = 'jpeg'
+            extension = 'jpg'
+        elif img_type == 'image/png':
+            pil_type = 'png'
+            extension = 'png'
+
+        image = Image.open(BytesIO(self.image.read()))
+
+        thumb_size = (400, 400)
+
+        image.thumbnail(thumb_size, Image.ANTIALIAS)
+
+        temp = BytesIO()
+        image.save(temp, pil_type)
+        temp.seek(0)
+
+        thumbnail = SimpleUploadedFile(os.path.split(self.image.name)[-1], temp.read(), content_type=img_type)
+
+        self.thumbnail.save(
+            '%s_thumbnail.%s' % (os.path.splitext(thumbnail.name)[0], extension),
+            thumbnail,
+            save=False
+        )
+
+
+class PhotoAlbum(models.Model):
+
+    title = models.CharField(max_length=64,)
+
+    cover = models.ForeignKey(Photo,
+                              null=True,
+                              blank=True,
+                              on_delete=models.SET_NULL)
+
+    description = models.TextField(null=True,
+                                   blank=True,
+                                   verbose_name='Opis')
+
+    added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-added"]
+
+    def __str__(self):
+        return self.title
--- zajazd_website/gallery/views.py
+++ zajazd_website/gallery/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import PhotoAlbum, Photo
 
-# Create your views here.
+
+class Albums(ListView):
+    model = PhotoAlbum
+    template_name = 'gallery/albums.html'
+    context_object_name = 'albums'
+
+
+class Gallery(DetailView):
+    model = PhotoAlbum
+    pk_url_kwarg = 'album_id'
+    template_name = 'gallery/gallery.html'
+    context_object_name = 'album'
+
+
+class PhotoView(DetailView):
+    model = Photo
+    pk_url_kwarg = 'photo_id'
+    template_name = 'gallery/photo.html'
